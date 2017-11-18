123b449f3aa9c622f2c642dad708b1cdf0cf3d3a
konstantinaxt <axtkonstantin@hotmail.de>
2017-11-18 12:07:32 +0100

    fix header path compare

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,24 +1,28 @@
 import Link from 'gatsby-link'
 import React from 'react'
 import PropTypes from 'prop-types'
+import {isEqual, chain} from 'lodash';
 import "./header.css";
 import logo from "./../../../static/img/logo.svg";
 
 const Header = ({links, toggleSubMenu, activeSubmenuClass, pathname}) => {
 
-  console.log("pathname: ", pathname);
-
   return (
     <header className={`header ${activeSubmenuClass}`}>
-      <div className="logo"><img src={logo}/></div>
+      <div className="logo"><img src={logo} /></div>
       <div onClick={toggleSubMenu} className="burger">
         <i className="fa fa-bars" aria-hidden="true" />
       </div>
       <nav id="menu" className="menu">
         <ul className="menu_container">
           {links.map((link) => {
+            const path = chain(pathname).split("/").compact().value();
+            const linkTo = chain(link.to).split("/").compact().value();
+
+            let isActive = isEqual(path, linkTo);
+
             return (
-              <li className={pathname === link.to ? "active" : null} key={link.to}>
+              <li className={isActive ? "active" : null} key={link.to}>
                 <Link to={link.to}>{link.name}</Link>
               </li>
             )
