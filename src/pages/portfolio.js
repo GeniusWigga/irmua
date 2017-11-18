11f18f0e6ecb4a43dfa9a9248076ee49c4ef7e38
konstantinaxt <axtkonstantin@hotmail.de>
2017-11-18 11:19:50 +0100

    add images for portfolio

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,16 +1,38 @@
 import React, {Component} from 'react'
-import Masonry from "masonry-layout";
-import imagesloaded from "imagesloaded";
+import brideImage1 from "../../static/img/portfolio/bride/bride_4.jpg";
+import brideImage2 from "../../static/img/portfolio/bride/bride_3.jpg";
+import brideImage3 from "../../static/img/portfolio/bride/bride_5.jpg";
+import brideImage4 from "../../static/img/portfolio/bride/bride_2.jpg";
+import brideImage6 from "../../static/img/portfolio/bride/bride_6.jpg";
+import brideImage7 from "../../static/img/portfolio/bride/bride_7.jpg";
 
-import brideImage1 from "./../../static/img/portfolio/4.jpg";
-import brideImage2 from "./../../static/img/portfolio/3.jpg";
-import brideImage3 from "./../../static/img/portfolio/5.jpg";
-import brideImage4 from "./../../static/img/portfolio/2.jpg";
+import haircut1 from "../../static/img/portfolio/haircut/haircut_1.jpg";
+import haircut2 from "../../static/img/portfolio/haircut/haircut_2.jpg";
+import haircut3 from "../../static/img/portfolio/haircut/haircut_3.jpg";
+import haircut4 from "../../static/img/portfolio/haircut/haircut_4.jpg";
+
+import makeup1 from "../../static/img/portfolio/makeup/makeup_1.jpg";
+import makeup2 from "../../static/img/portfolio/makeup/makeup_2.jpg";
+import makeup3 from "../../static/img/portfolio/makeup/makeup_3.jpg";
+import makeup4 from "../../static/img/portfolio/makeup/makeup_4.jpg";
+
+import shooting1 from "../../static/img/portfolio/shooting/shooting_1.jpg";
+import shooting2 from "../../static/img/portfolio/shooting/shooting_2.jpg";
+import shooting3 from "../../static/img/portfolio/shooting/shooting_3.jpg";
+import shooting4 from "../../static/img/portfolio/shooting/shooting_4.jpg";
+
+import beforeAfter1 from "../../static/img/portfolio/beforeAfter/beforeAfter_1.jpg";
+import beforeAfter2 from "../../static/img/portfolio/beforeAfter/beforeAfter_2.jpg";
+import beforeAfter3 from "../../static/img/portfolio/beforeAfter/beforeAfter_3.jpg";
+import beforeAfter4 from "../../static/img/portfolio/beforeAfter/beforeAfter_4.jpg";
 
 export default class Portfolio extends Component {
 
   componentDidMount() {
     if (typeof window !== 'undefined' && typeof document !== 'undefined') {
+      const Masonry = require("masonry-layout");
+      const imagesloaded = require("imagesloaded");
+
       const grid = document.querySelector('.grid');
 
       const msnry = new Masonry(grid, {
@@ -22,7 +44,6 @@ export default class Portfolio extends Component {
       imagesloaded(grid).on('progress', function () {
         msnry.layout();
       });
-
     }
   }
 
@@ -34,7 +55,25 @@ export default class Portfolio extends Component {
           <div className="grid-item"><img src={brideImage1} /></div>
           <div className="grid-item"><img src={brideImage3} /></div>
           <div className="grid-item"><img src={brideImage2} /></div>
+          <div className="grid-item"><img src={brideImage6} /></div>
           <div className="grid-item"><img src={brideImage4} /></div>
+          <div className="grid-item"><img src={brideImage7} /></div>
+          <div className="grid-item"><img src={haircut1} /></div>
+          <div className="grid-item"><img src={haircut2} /></div>
+          <div className="grid-item"><img src={haircut3} /></div>
+          <div className="grid-item"><img src={haircut4} /></div>
+          <div className="grid-item"><img src={makeup1} /></div>
+          <div className="grid-item"><img src={makeup2} /></div>
+          <div className="grid-item"><img src={makeup3} /></div>
+          <div className="grid-item"><img src={makeup4} /></div>
+          <div className="grid-item"><img src={shooting1} /></div>
+          <div className="grid-item"><img src={shooting2} /></div>
+          <div className="grid-item"><img src={shooting3} /></div>
+          <div className="grid-item"><img src={shooting4} /></div>
+          <div className="grid-item"><img src={beforeAfter1} /></div>
+          <div className="grid-item"><img src={beforeAfter2} /></div>
+          <div className="grid-item"><img src={beforeAfter3} /></div>
+          <div className="grid-item"><img src={beforeAfter4} /></div>
         </div>
       </div>
     );
